259aef4fcc1df58bf977d8235e7b36c051efaf45
Ivy <wenxun1987@126.com>
2017-03-07 09:02:58 +0800

    获取当前天气

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+	$.ajax({
+		url: 'https://free-api.heweather.com/v5/now?key=4f29a6d59fe14939a7c48cbce64190ca&city=beijing',
+		type: 'get',
+		dataType: 'json',
+		success: function(data){
+			var cond = data.HeWeather5[0].now.cond.txt;
+			var tmp = data.HeWeather5[0].now.tmp;
+			var txt1 = $('<span></span>');
+			var txt2 = $('<span></span>');
+			txt1.text(cond + ' ').appendTo($('.weather'));
+			txt2.text(tmp + '℃').appendTo($('.weather'));
+
+		}
+	})
+
+
+});
